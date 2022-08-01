7c251b72af2cd191845d56d6cbe333a46f936139
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-01 12:15:41 -0300

    rq 15

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,1 @@
-db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
-db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
-db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
-db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 }); 
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
