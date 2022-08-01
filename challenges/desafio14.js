5efd5fe3049de56ee8020c4183e285433caccb42
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-01 12:13:21 -0300

    rq 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,3 +1,4 @@
-db.produtos.find({ ingredientes:
-  { $eq: "picles" },
-}, { nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 }, _id: 0 }); 
+db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 }); 
